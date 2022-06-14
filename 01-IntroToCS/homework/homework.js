c5aefd0da87de968b25c4f10fcebb4a0f31ba715
Mijail Pulgar <mijail86@gmail.com>
2022-06-14 07:15:48 -0500

    Homework intro resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,28 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    result += Number(num[i]) * Math.pow(2, num.length - i - 1);
+    
+  }
+  return result;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let result = [];
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num = Math.floor(num / 2);
+      result.unshift("0");
+    } else {
+      num = Math.floor(num / 2);
+      result.unshift("1");
+    }
+  }
+  return result.join("");
 }
 
 
