fc7d64d88b440b6136a7873f530e5fd855578cbe
mijail pulgar <mijail86@gmail.com>
2022-07-07 14:54:49 -0500

    hw algoritmos listo

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,12 +1,38 @@
 'use strict'
 // No cambies los nombres de las funciones.
 
-function factorear(num) {
+function factorear(num, arr = [1], divisor = 2) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  // let arr = [1];
+  // let divisor = 2;
+  // while(num !== 1){
+  //   if (num % divisor === 0) {
+  //     arr.push(divisor);
+  //     num = num / divisor;
+  //   }else{
+  //     divisor++;
+  //   }
+  // }
+  // return arr;
+
+  if(num === 1){
+    return arr;
+  }else{
+    if(num % divisor === 0){
+      arr.push(divisor);
+      return factorear(num / divisor, arr, divisor);
+    }else{
+      while(num % divisor !== 0){
+        divisor++
+      }
+      arr.push(divisor);
+    }
+    return factorear(num/divisor, arr, divisor)
+  }
 }
 
 function bubbleSort(array) {
@@ -14,7 +40,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1] ) {
+        let aux = array[j];
+        array[j] = array[j+1];
+        array[j+1] = aux
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +58,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  const {length} = array;
+  let temp;
+  for (let i = 1; i < length; i++) {
+    let j = i, temp = array[i];
+    while (j > 0 && array[j - 1] > temp) {
+      array[j] = array[j - 1];
+      j--;
+    }
+    array[j] = temp;
+  }
+  return array;
 }
 
 
@@ -32,7 +77,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let indexMin;
+  for (let i = 0; i < array.length - 1; i++) {
+    indexMin = i;
+    for (let j = i; j < array.length; j++) {
+      if (array[indexMin] > array[j]) {
+        indexMin = j;
+      }
+    }
+    if (i !== indexMin) {
+      let aux = array[i];
+      array[i] = array[indexMin];
+      array[indexMin] = aux;
+    }
+  }
+  return array;
 }
 
 
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,21 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length > 1) {
+    let random = Math.round(Math.random() * array.length);
+    let pivot = array.splice(random, 1);
+    let smaller = [], bigger = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > pivot) {
+        bigger.push(array[i]);
+      }else{
+        smaller.push(array[i]);
+      }
+    }
+    return quickSort(smaller).concat(pivot).concat(quickSort(bigger));
+  }else{
+    return array;
+  }
 }
 
 function mergeSort(array) {
@@ -14,7 +28,37 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  function merge(a, b) {
+    let c = [];
+    while (a.length && b.length > 0) {
+      if (a[0] > b[0]) {
+        c.push(b[0])
+        b.splice(0,1)
+      } else {
+        c.push(a[0])
+        a.splice(0,1)
+      }
+    }
+    while (a.length > 0) {
+      c.push(a[0])
+      a.splice(0,1)
+    }
+    while (b.length > 0) {
+      c.push(b[0])
+      b.splice(0,1)
+    }
+    return c;
+  }
 
+  if(array.length > 1){
+    let middle = Math.floor((array.length) / 2);
+    let one = array.slice(0, middle),
+    two = array.slice(middle, array.length);
+    one = mergeSort(one), two = mergeSort(two);
+    return merge(one, two);
+  }else{
+    return array;
+  }
 }
 
 // No modificar nada debajo de esta línea
